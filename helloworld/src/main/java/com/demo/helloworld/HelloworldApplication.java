ef1b681a2e0c62b281c0cacafb6a2286eb0616a2
swaggyTech <122125597@qq.com>
2020-04-15 17:53:15 +0800

    springboot-demo 1.helloworld

---
+++ helloworld/src/main/java/com/demo/helloworld/HelloworldApplication.java
@@ -0,0 +1,26 @@
+package com.demo.helloworld;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(required = false,name = "who")String who){
+        if(StrUtil.isBlank(who)){
+            who = "World";
+        }
+        return StrUtil.format("Hello,{}",who);
+    }
+
+}
